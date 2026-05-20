6c7c16c4b578ff9e5098b6eba95e3147e45e7340
Dekdark4 <zzkskl@gmail.com>
2026-05-20 13:36:22 +0300

    бернул программу в while-loop и наметил простейшую структуру алгоритма.

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,51 @@ enum class MenuOptions
 
 int main()
 {
-	std::cout << "========= MAIN MENU =========" << std::endl;
-	std::cout << std::setw(14) << std::left << "Get problem:" << std::setw(5) << std::right << "[1]" << std::endl;
-	std::cout << std::setw(14) << std::left << "Add problem:" << std::setw(5) << std::right << "[2]" << std::endl;
-	std::cout << std::setw(14) << std::left << "Exit program:" << std::setw(5) << std::right << "[0]" << std::endl << std::endl;
-	std::cout << "Enter your option:  ";
 
-	std::string option_str{};
-	std::getline(std::cin, option_str);
-	uint16_t option{};
+
+
+	while (true)
+	{
+		// ===================== ОТРИСОВКА МЕНЮ ======================
+		std::cout << "========= MAIN MENU =========" << std::endl;
+		std::cout << std::setw(14) << std::left << "Get problem:" << std::setw(5) << std::right << "[1]" << std::endl;
+		std::cout << std::setw(14) << std::left << "Add problem:" << std::setw(5) << std::right << "[2]" << std::endl;
+		std::cout << std::setw(14) << std::left << "Exit program:" << std::setw(5) << std::right << "[0]" << std::endl << std::endl;
+		std::cout << "Enter your option:  ";
+		// ===========================================================
+
+
+
+		// ===================== ПОЛУЧЕНИЕ ВВОДА =====================
+		std::string option_str{};
+		std::getline(std::cin, option_str);
+		// ===========================================================
+		
+
+
+		// ====================== ПАРСИНГ ВВОДА ======================
+		// ===========================================================
+		
+
+
+		// ======= ПРЕОБРАЗОВАНИЕ В enum И ВЫПОЛНЕНИЕ ДЕЙСТВИЯ =======
+		MenuOptions choice;
+
+		switch (choice)
+		{
+
+		}
+		// ===========================================================
+		
+
+
+		// ========================== ПАУЗА ==========================
+		std::cin.get();
+		system("cls");
+		// ===========================================================
+	}
+
+
 
 	std::cin.get();
 	return 0;
