98430fa75205eb10a197f51042408a9e1856ab7a
Dekdark4 <zzkskl@gmail.com>
2026-05-20 13:17:55 +0300

    Создал простейший прототип меню (без while-loop)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 ﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdint>
 
 int main()
 {
+	std::cout << "========= MAIN MENU =========" << std::endl;
+	std::cout << std::setw(14) << std::left << "Get problem:" << std::setw(5) << std::right << "[1]" << std::endl;
+	std::cout << std::setw(14) << std::left << "Add problem:" << std::setw(5) << std::right << "[2]" << std::endl;
+	std::cout << std::setw(14) << std::left << "Exit program:" << std::setw(5) << std::right << "[0]" << std::endl << std::endl;
+	std::cout << "Enter your option:  ";
+
+	std::string option_str{};
+	std::getline(std::cin, option_str);
+	uint16_t option{};
 
 	std::cin.get();
 	return 0;
