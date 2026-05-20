e78db01fa4065e55c96f0c3251d82f0fea387c52
Dekdark4 <zzkskl@gmail.com>
2026-05-20 16:15:29 +0300

    азобрался с файлами. Создал: StringParser.h и StringParser.cpp. аписал простой парсер.

---
+++ include/StringParser.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <cstdint>
+#include <string>
+#include <optional>
+
+namespace Parser
+{
+
+	std::optional<uint16_t> try_parse_uint(const std::string& input);
+
+}
---
+++ src/StringParser.cpp
@@ -0,0 +1,27 @@
+﻿#include "StringParser.h"
+#include <sstream>
+#include <limits>
+
+std::optional<uint16_t> Parser::try_parse_uint(const std::string& input)
+{
+	// Пустая ли строка
+	if (input.empty())
+		return std::nullopt;
+
+	// Содержит ли строка число
+	std::istringstream iss(input);
+	long long temp{};
+	if (!(iss >> temp))
+		return std::nullopt;
+	
+	// Нету ли мусора
+	if (!(iss >> std::ws).eof())
+		return std::nullopt;
+
+	// Находится ли число в нужном диапазоне
+	if (temp < std::numeric_limits<uint16_t>::min() || temp > std::numeric_limits<uint16_t>::max())
+		return std::nullopt;
+
+	// Единственно верный исход
+	return static_cast<uint16_t>(temp);
+}
