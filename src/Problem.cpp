e5484f784302b680eaa34483bf7538e6feb583a9
Dekdark4 <zzkskl@gmail.com>
2026-06-05 17:17:55 +0300

    Добавил простейшую реализацию add_problem(): проверка на открытие файла, запись в файл.

--- src/Problem.cpp
+++ src/Problem.cpp
@@ -1,7 +1,19 @@
 ﻿#include "Problem.h"
+#include <iostream>
 #include <fstream>
 
 void Problem::add_problem(std::string problem)
 {
+	std::ofstream all_problems_txt;
+	all_problems_txt.open(PATH, std::ofstream::app);
 
+	if (!all_problems_txt.is_open())
+	{
+		system("cls");
+		std::cerr << "The file has not opened... [press enter]";
+		std::cin.get();
+		return;
+	}
+	
+	all_problems_txt << "\n" + problem;
 }
